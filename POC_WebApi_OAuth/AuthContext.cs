add1446f40a9d8c19a028a800a3dba0381375b14
0001vrn <vrnthkr969@gmail.com>
2018-03-10 13:13:11 +0530

    first commit

---
+++ POC_WebApi_OAuth/AuthContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POC_WebApi_OAuth
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext")
+        {
+
+        }
+    }
+}
